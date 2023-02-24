3d0179667aa8fcb678916903d7a4fb74399d5874
Carl <molna018@umn.edu>
2023-02-24 12:51:21 +0200

    experiment changes

--- experiments/train_crafter.py
+++ experiments/train_crafter.py
@@ -10,15 +10,15 @@ from dreamerv2.train_with_configs import main
 logdir_path = r'C:\Users\Carl\OneDrive\Desktop'
 task = 'crafter_reward'
 ssm_type = 'rssm_em'
-exp_name = '_exp'
+exp_name = ''
 
 config = dv2.defaults.update({
     'task': task,
-    'jit': False,
+    'jit': True,
     'time_limit': 100,
     'eval_every': 100,
     'log_every': 100,
-    'prefill': 100,
+    'prefill': 10000,
     'pretrain': 1,
     'train_steps': 1,
     # 'logdir': f'{logdir_path}/logdir/{task}/{ssm_type}',
@@ -39,12 +39,12 @@ config = dv2.defaults.update({
     'model_opt.lr': 1e-4,
     'actor_opt.lr': 1e-4,
     'critic_opt.lr': 1e-4,
-    '.*\.norm': 'layer',
+    # '.*\.norm': 'layer',
 
     'episodic_memory.max_size': 128,
     'dataset.batch': 8, 
     'dataset.length': 50,
-    'load_model': False
+    'load_model': True
 }).parse_flags()
 
 main(config)
